736a46876ec47508083d1a8b30db0e5932d464b9
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-19 21:01:04 +0100

    refactor(layout): render GameGrid

--- src/components/layout/Applayout.tsx
+++ src/components/layout/Applayout.tsx
@@ -1,10 +1,14 @@
+import GameGrid from '@/components/features/GameGrid';
+
 const Applayout = () => {
   return (
     <div className='flex'>
       <aside className='hidden lg:block w-64'>
-        <div>side bar</div>
+        <div className='min-h-screen'>side bar</div>
       </aside>
-      <main className='flex-1'>Game Grid</main>
+      <main className='min-h-screen flex-1'>
+        <GameGrid />
+      </main>
     </div>
   );
 };
