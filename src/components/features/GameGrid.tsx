8db93e16007bda5ae6d1e0046162bf6642f4f805
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-19 04:53:12 +0100

    refactor(games): extract API logic into useGames hook

--- src/components/features/GameGrid.tsx
+++ src/components/features/GameGrid.tsx
@@ -1,28 +1,9 @@
-import { useState, useEffect } from 'react';
-import apiClient from '@/services/api-client';
+import useGames from '@/hooks/useGames';
 import { AlertCircleIcon } from 'lucide-react';
 import { Alert, AlertAction, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
-
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>('/games')
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
