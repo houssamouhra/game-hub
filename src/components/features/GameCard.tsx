649379129b86474f95b9086fb75bd95f8c6fa890
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-20 20:29:17 +0100

    feat(games): render CriticScore in game cards

--- src/components/features/GameCard.tsx
+++ src/components/features/GameCard.tsx
@@ -1,6 +1,7 @@
 import { type Game } from '@/hooks/useGames';
 import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 import PlatformIconList from '@/components/features/PlatformIconList';
+import CriticScore from '@/components/features/CriticScore';
 
 interface GameCardProps {
   game: Game;
@@ -14,7 +15,12 @@ const GameCard = ({ game }: GameCardProps) => {
       </div>
       <CardHeader>
         <CardTitle className='text-xl'>{game.name}</CardTitle>
-        <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+        <div className='flex justify-between items-center'>
+          <div className='flex gap-1'>
+            <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+          </div>
+          <CriticScore score={game.metacritic} />
+        </div>
       </CardHeader>
     </Card>
   );
