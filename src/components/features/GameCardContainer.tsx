571030d3fd9f8c28e85ec38b448b236c61f71e44
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-21 00:58:13 +0100

    feat(ui): introduce GameCardContainer layout component

---
+++ src/components/features/GameCardContainer.tsx
@@ -0,0 +1,18 @@
+import { Card } from '@/components/ui/card';
+import { type ReactNode } from 'react';
+
+interface GameCardContainerProps {
+  image: ReactNode;
+  header: ReactNode;
+}
+
+const GameCardContainer = ({ image, header }: GameCardContainerProps) => {
+  return (
+    <Card size='sm' className='h-full w-full'>
+      {image}
+      {header}
+    </Card>
+  );
+};
+
+export default GameCardContainer;
