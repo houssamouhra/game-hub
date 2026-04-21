4f2ae3102ea2d622a70090d627d7a3761db549af
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-21 16:05:01 +0100

    refactor(genres): migrate useGenres to useData hook

--- src/components/features/GenreList.tsx
+++ src/components/features/GenreList.tsx
@@ -1,12 +1,12 @@
 import useGenres from '@/hooks/useGenres';
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
 
   return (
     <div className='min-h-screen pt-10'>
       <ul>
-        {genres.map((genre) => (
+        {data.map((genre) => (
           <li key={genre.id}>{genre.name}</li>
         ))}
       </ul>
