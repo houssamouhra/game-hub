50f4b19e676013f7aeae73b04a5663c175c23e29
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-21 01:03:03 +0100

    refactor(games): migrate GameCard to GameCardContainer layout

--- src/components/features/GameCard.tsx
+++ src/components/features/GameCard.tsx
@@ -1,5 +1,6 @@
 import { type Game } from '@/hooks/useGames';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { CardHeader, CardTitle } from '@/components/ui/card';
+import GameCardContainer from '@/components/features/GameCardContainer';
 import PlatformIconList from '@/components/features/PlatformIconList';
 import CriticScore from '@/components/features/CriticScore';
 import getCroppedImageUrl from '@/utils/image-url';
@@ -10,18 +11,26 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card>
-      <img src={getCroppedImageUrl(game.background_image)} alt='game_image' />
-      <CardHeader>
-        <CardTitle className='text-xl'>{game.name}</CardTitle>
-        <div className='flex justify-between items-center'>
-          <div className='flex gap-1'>
-            <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+    <GameCardContainer
+      image={
+        <img
+          src={getCroppedImageUrl(game.background_image)}
+          alt='game-image'
+          className='w-full h-full object-cover'
+        />
+      }
+      header={
+        <CardHeader>
+          <CardTitle className='text-xl'>{game.name}</CardTitle>
+          <div className='flex justify-between items-center'>
+            <div className='flex gap-1'>
+              <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+            </div>
+            <CriticScore score={game.metacritic} />
           </div>
-          <CriticScore score={game.metacritic} />
-        </div>
-      </CardHeader>
-    </Card>
+        </CardHeader>
+      }
+    />
   );
 };
 
