1ba6dd62935795ca09d073e60ed8162e385d6444
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-21 01:04:17 +0100

    refactor(games): migrate GameCardSkeleton to GameCardContainer layout

--- src/components/features/GameCardSkeleton.tsx
+++ src/components/features/GameCardSkeleton.tsx
@@ -1,22 +1,27 @@
-import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
+import { CardContent, CardHeader } from '@/components/ui/card';
+import GameCardContainer from '@/components/features/GameCardContainer';
 
 const GameCardSkeleton = () => {
   return (
-    <Card className='h-77.5'>
-      <CardContent>
-        <Skeleton className='aspect-video md:aspect-3/2 w-full' />
-      </CardContent>
-      <CardHeader className='flex justify-between items-end'>
-        <div className='flex flex-col gap-2'>
-          <Skeleton className='h-4 w-40' />
-          <Skeleton className='h-4 w-2/3' />
-        </div>
-        <div className='shrink-0'>
-          <Skeleton className='h-4 w-8' />
-        </div>
-      </CardHeader>
-    </Card>
+    <GameCardContainer
+      image={
+        <CardContent>
+          <Skeleton className='aspect-video' />
+        </CardContent>
+      }
+      header={
+        <CardHeader className='flex justify-between items-end'>
+          <div className='flex flex-col gap-2'>
+            <Skeleton className='h-4 w-40' />
+            <Skeleton className='h-4 w-20' />
+          </div>
+          <div className='shrink-0'>
+            <Skeleton className='h-4 w-8' />
+          </div>
+        </CardHeader>
+      }
+    />
   );
 };
 
