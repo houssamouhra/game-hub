64b5ee670dd65dd5f56079ee4972017339a81e1f
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-21 23:28:04 +0100

    feat(genres): show skeleton while loading

--- src/components/features/GenreList.tsx
+++ src/components/features/GenreList.tsx
@@ -1,24 +1,27 @@
 import useGenres from '@/hooks/useGenres';
 import getCroppedImageUrl from '@/utils/image-url';
+import GenreListSkeleton from '@/components/features/GenreListSkeleton';
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
 
   return (
     <div className='min-h-screen pt-10'>
       <ul>
-        {data.map((genre) => (
-          <li key={genre.id}>
-            <div className='flex items-center gap-1.5 py-1'>
-              <img
-                src={getCroppedImageUrl(genre.image_background)}
-                alt={genre.name}
-                className='w-8 h-8 rounded-md object-cover'
-              />
-              <div className='text-md'>{genre.name}</div>
-            </div>
-          </li>
-        ))}
+        {isLoading
+          ? Array.from({ length: 10 }, (_, index) => <GenreListSkeleton key={index} />)
+          : data.map((genre) => (
+              <li key={genre.id}>
+                <div className='flex items-center gap-1.5 py-1'>
+                  <img
+                    src={getCroppedImageUrl(genre.image_background)}
+                    alt={genre.name}
+                    className='w-8 h-8 rounded-md object-cover'
+                  />
+                  <div className='text-md'>{genre.name}</div>
+                </div>
+              </li>
+            ))}
       </ul>
     </div>
   );
