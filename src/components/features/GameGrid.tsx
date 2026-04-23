e9e30c38c367ec6418f556530e98862d5360906c
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-23 20:06:46 +0100

    refactor(ui): update GameGrid error message structure

--- src/components/features/GameGrid.tsx
+++ src/components/features/GameGrid.tsx
@@ -1,6 +1,6 @@
 import { Icon } from '@iconify/react';
 import { GameCard, GameCardSkeleton } from '@/components/features/GameCard';
-import { Alert, AlertAction, AlertDescription, AlertTitle } from '@/ui/alert';
+import { Alert, AlertDescription, AlertTitle } from '@/ui/alert';
 import useGames, { type Platform } from '@/hooks/useGames';
 import { type Genre } from '@/hooks/useGenres';
 
@@ -15,12 +15,13 @@ const GameGrid = ({ selectedGenre, selectedPlatform }: GameGridProps) => {
   return (
     <>
       {error && (
-        <Alert variant='destructive' className='max-w-md'>
-          <Icon icon='tabler:alert-circle' width='4' height='4' />
-          <AlertTitle>Something went wrong</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-          <AlertAction></AlertAction>
-        </Alert>
+        <div className='max-w-sm pl-10'>
+          <Alert variant='destructive'>
+            <Icon icon='tabler:alert-circle' />
+            <AlertTitle>Something went wrong</AlertTitle>
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
+        </div>
       )}
       <div className='grid grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-10 pt-0 gap-6'>
         {isLoading && Array.from({ length: 20 }, (_, i) => <GameCardSkeleton key={i} />)}
