4a3c818dbc84ce6b62726b05d036b5397e33ee0c
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-28 02:07:12 +0100

    style(critic-score): refine badge colors for light and dark themes

--- src/components/features/CriticScore.tsx
+++ src/components/features/CriticScore.tsx
@@ -7,9 +7,9 @@ interface CriticScoreProps {
 const CriticScore = ({ score }: CriticScoreProps) => {
   const color =
     score > 75
-      ? 'text-green-800 dark:text-green-300 bg-green-100 dark:bg-green-950'
+      ? 'text-green-800 bg-green-100 dark:text-green-300 dark:bg-green-950'
       : score > 60
-        ? 'text-yellow-300 bg-yellow-950'
+        ? 'text-yellow-800 bg-yellow-100 dark:text-yellow-300 dark:bg-yellow-950 '
         : '';
   return (
     <Badge variant='secondary' className={color}>
