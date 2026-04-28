6a3c6840d91d0dbba8c7dcd5ce1c044f50788bc5
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-28 02:01:31 +0100

    refactor(GenreList): add heading and enhance styling

--- src/components/features/GenreList/GenreList.tsx
+++ src/components/features/GenreList/GenreList.tsx
@@ -11,36 +11,37 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   const { data, isLoading } = useGenres();
 
   return (
-    <div className='min-h-screen mt-10'>
-      <ul>
-        {isLoading
-          ? Array.from({ length: 10 }, (_, index) => <GenreListSkeleton key={index} />)
-          : data.map((genre) => {
-              const isActive = selectedGenre?.id === genre.id;
+    <>
+      <h1 className='text-2xl font-semibold ml-2 my-3 tracking-tight text-balance'>Genres</h1>
+      <div className='min-h-screen mt-2'>
+        <ul>
+          {isLoading
+            ? Array.from({ length: 10 }, (_, index) => <GenreListSkeleton key={index} />)
+            : data.map((genre) => {
+                const isActive = selectedGenre?.id === genre.id;
 
-              return (
-                <li key={genre.id}>
-                  <div
-                    onClick={() => onSelectGenre(genre)}
-                    className={`flex items-center gap-1.5 py-1 px-2 rounded-md cursor-pointer transition-colors ${
-                      isActive
-                        ? 'text-white bg-zinc-900 font-semibold'
-                        : 'text-gray-400 hover:text-white'
-                    }`}
-                  >
-                    {' '}
-                    <img
-                      src={getCroppedImageUrl(genre.image_background)}
-                      alt={genre.name}
-                      className='w-8 h-8 rounded-md object-cover'
-                    />
-                    <div>{genre.name}</div>
-                  </div>
-                </li>
-              );
-            })}
-      </ul>
-    </div>
+                return (
+                  <li key={genre.id}>
+                    <button
+                      onClick={() => onSelectGenre(genre)}
+                      className={`flex items-center gap-1.5 py-1 px-2 rounded-md text-left cursor-pointer transition-colors ${
+                        isActive ? 'font-semibold' : 'hover:font-semibold'
+                      }`}
+                    >
+                      {' '}
+                      <img
+                        src={getCroppedImageUrl(genre.image_background)}
+                        alt={genre.name}
+                        className='w-8 h-8 rounded-md object-cover'
+                      />
+                      <div>{genre.name}</div>
+                    </button>
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
+    </>
   );
 };
 
