ede3fb0c4d3d39720457f24a77eb17ed1e7848e5
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-29 21:47:57 +0100

    refactor(game-grid): clean up render structure

--- src/components/features/game/GameGrid.tsx
+++ src/components/features/game/GameGrid.tsx
@@ -14,23 +14,23 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
 
   const showSkeleton = isLoading || data.length === 0;
 
-  return (
-    <>
-      {error && (
-        <div className='max-w-sm pl-10'>
-          <Alert variant='destructive'>
-            <Icon icon='tabler:alert-circle' />
-            <AlertTitle>Something went wrong</AlertTitle>
-            <AlertDescription>{error}</AlertDescription>
-          </Alert>
-        </div>
-      )}
-      <div className='grid grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-10 pt-0 gap-6'>
-        {showSkeleton
-          ? Array.from({ length: 20 }, (_, i) => <GameCardSkeleton key={i} />)
-          : data.map((game) => <GameCard key={game.id} game={game} />)}
+  if (error)
+    return (
+      <div className='max-w-sm pl-10'>
+        <Alert variant='destructive'>
+          <Icon icon='tabler:alert-circle' />
+          <AlertTitle>Something went wrong</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
       </div>
-    </>
+    );
+
+  return (
+    <div className='grid grid-rows-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-10 pt-0 gap-6'>
+      {showSkeleton
+        ? Array.from({ length: 20 }, (_, i) => <GameCardSkeleton key={i} />)
+        : data.map((game) => <GameCard key={game.id} game={game} />)}
+    </div>
   );
 };
 
