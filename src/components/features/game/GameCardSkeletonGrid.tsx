c619091af0adbadc5c21eb696453080b9fa838a1
Houssam Ouhra <houssamblur04@gmail.com>
2026-05-11 20:25:38 +0100

    feat: add reusable game card skeleton grid

---
+++ src/components/features/game/GameCardSkeletonGrid.tsx
@@ -0,0 +1,17 @@
+import GameCardSkeleton from '@/features/game/GameCardSkeleton';
+
+interface GameCardSkeletonGridProps {
+  count?: number;
+}
+
+const GameCardSkeletonGrid = ({ count = 20 }: GameCardSkeletonGridProps) => {
+  return (
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6'>
+      {Array.from({ length: count }, (_, i) => (
+        <GameCardSkeleton key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default GameCardSkeletonGrid;
