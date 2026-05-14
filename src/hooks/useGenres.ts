edc673d4f9ced07ab97d923708dab9faa419e6e9
Houssam Ouhra <houssamblur04@gmail.com>
2026-05-14 04:09:52 +0100

    refactor: update genres data structure and simplify initialData in useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -14,8 +14,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: { count: genres.length, results: genres, next: null },
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: genres,
   });
 
 export default useGenres;
