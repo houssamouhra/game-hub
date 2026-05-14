6d3e79fe1b8955a2dc58ca1dda98ff37f5c36f6d
Houssam Ouhra <houssamblur04@gmail.com>
2026-05-14 04:36:10 +0100

    refactor: replace staleTime constant with ms utility in usePlatforms hook

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,3 +1,4 @@
+import ms from 'ms';
 import { useQuery } from '@tanstack/react-query';
 import APIClient from '@/services/api-client';
 import platforms from '@/data/platforms';
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
     initialData: platforms,
   });
 
